01e7fda1a3cc0064e2237a91c6521f94a8a8c36e
gundars-o <gundars.ozolins75@gmail.com>
2021-01-24 17:12:58 +0200

    Line added.

--- src/index.js
+++ src/index.js
@@ -23,13 +23,25 @@ const material = new THREE.MeshBasicMaterial(
 );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+// Add line
+const materialLine = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const pointsLine = [];
+pointsLine.push( new THREE.Vector3( -1, 0, 0 ) );
+pointsLine.push( new THREE.Vector3( 0, 3, 0 ) );
+pointsLine.push( new THREE.Vector3( 5, 0, 0 ) );
+const geometryLine = new THREE.BufferGeometry().setFromPoints( pointsLine );
+const line = new THREE.Line( geometryLine, materialLine );
+scene.add( line );
 // Move the camera out a bit from coordinates (0,0,0).
-camera.position.z = 5;
-// Animate defined cube.
+// camera.position.z = 5;
+camera.position.set( 0, 0, 6 );
+camera.lookAt( 0, 3, 0 );
+// Animate defined elements.
 const animate = function() {
     requestAnimationFrame( animate );
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    line.rotation.y += 0.01 /* rotate around y (vertical) axis */;
     renderer.render( scene, camera );
 };
 animate();
